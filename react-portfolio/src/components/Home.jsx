8d6e86f0a4e6c5637706cfbd317a391295f5e328
javiervargas <javiervg1802@gmail.com>
2022-08-13 20:26:50 -0500

    Home file import to app.js

---
+++ react-portfolio/src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {HiArrowNarrowRight} from 'react-icons/hi'
+
+
+const Home = () => {
+  return (
+    <div className='bg-[#10162f]'>
+
+    </div>
+  )
+}
+
+export default Home
