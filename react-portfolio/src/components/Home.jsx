59a06fc8e06347b3e62049d99fb399bd614a6876
javiervargas <javiervg1802@gmail.com>
2022-08-13 22:47:16 -0500

    adding github, facebook, twitter and link icons

--- react-portfolio/src/components/Home.jsx
+++ react-portfolio/src/components/Home.jsx
@@ -4,8 +4,16 @@ import {HiArrowNarrowRight} from 'react-icons/hi'
 
 const Home = () => {
   return (
-    <div className='bg-[#10162f]'>
-
+    <div name='home' className='w-full h-screen bg-[#10162f]'>
+        {/* container */}
+        <div className='nax-w-[100px] mx-auto px-8 flex flex-col justify-center h-full'> </div>
+        <p>Hey!, welcome to my portfolio created with react and tailwindcss</p>
+        <h1>My name is Javier Vargas</h1>
+        <h2>I'm a Full-Stack Develeoper who enjoys builds any type of apps
+        on this moment im trying to be better on some type of leguages for example: react, tailwind and node.</h2>
+        
+    
+    
     </div>
   )
 }
